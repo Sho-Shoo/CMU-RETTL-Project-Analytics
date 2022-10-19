a9b4dddac8bab6640444ea918eb28974fff0a864
Tianze Shou <shoutianze@outlook.com>
2022-10-19 17:22:54 -0400

    Wrote report for parameter sweep

--- temp.py
+++ temp.py
@@ -1,34 +1,7 @@
-import pandas as pd 
+import tutorDataAPI as tutorAPI 
 
-def getInRangePerc(file): 
+tutorLogDF = tutorAPI.getAnnotatedTutorLogDF("raw data/tutor_log.tsv") 
+period3StudentIDs = tutorAPI.getStudentIDsByPeriod(tutorLogDF, 4)
 
-    eventsDF = pd.read_csv(file) 
-    stopDF = eventsDF.loc[eventsDF["event"] == "Stopping"] 
-    notNAcount = sum(stopDF["subject"].notnull())
+print(period3StudentIDs)
 
-    return notNAcount / len(stopDF) 
-
-def getTableThreePerc(file): 
-
-    eventsDF = pd.read_csv(file) 
-    stopDF = eventsDF.loc[eventsDF["event"] == "Stopping"] 
-    tableThreeCount = stopDF["subject"].value_counts()[3]
-
-    return tableThreePerc / len(stopDF)
-
-if __name__ == "__main__": 
-
-    fileNames = ["event_master_file_D10_R500_RNG500_sprint2_shou.csv", 
-                 "event_master_file_D10_R500_RNG1000_sprint2_shou.csv", 
-                 "event_master_file_D10_R500_RNG2000_sprint2_shou.csv"] 
-    fileNames = "output_files/" + pd.Series(fileNames) 
-
-    for file in fileNames: 
-
-        inRangePerc = getInRangePerc(file) 
-        tableThreePerc = getTableThreePerc(file) 
-
-        print("In file", file) 
-        print("Percentage of stops that have student in-range is", inRangePerc)
-
-        print("**************************************************************")
